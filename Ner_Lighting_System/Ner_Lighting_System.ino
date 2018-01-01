e492123ed66c70096705b4d2186d036d72091e8f
chabad360 <chabad360@gmail.com>
2017-12-31 16:04:12 -0800

    Fixed a breaking issue where all LEDs but the first one wouldn't turn on.

--- Ner_Lighting_System/Ner_Lighting_System.ino
+++ Ner_Lighting_System/Ner_Lighting_System.ino
@@ -40,20 +40,19 @@ String num; // The LED we are trying to reach (used by the keypad).
 
 #define indicator A1 // The indicator LED.
 
-#define amount 51 // The amount of LEDs, necessary to turn them on.
+int amount = 51; // The amount of LEDs, necessary to turn them on.
 
-/* All our LEDs (in the addressable mode) */ int leds[amount] = { 53 /* This is LED number 1 but because we use serial, we need to have it at another pin, and 53 works */, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51};
+/* All our LEDs (in the addressable mode) */ int leds[51] = { 53 /* This is LED number 1 but because we use serial, we need to have it at another pin, and 53 works */, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 };
 
-/* All our LEDs (the way we type them) */ String nums[amount] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51" };
+/* All our LEDs (the way we type them) */ String nums[51] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", };
 
 void setup()
 {
 	// Initialize all LEDs
-	for (int i = 0; i <= amount; i++)
+	for (int i = 0; i <= 51; i++)
 	{
 		pinMode(leds[i], OUTPUT);
 		digitalWrite(leds[i], LOW);
-		break;
 	}
 
 	Serial.begin(9600); // Useful when you want to add outside communication to the system (e.x. testing, RPi, etc.)
@@ -175,12 +174,11 @@ void flash()
 */
 void state( String num, int HL )
 {
-	for (int i = 0; i <= amount; i++) // Run through all entries in 'nums'
+	for (int i = 0; i <= 51; i++) // Run through all entries in 'nums'
 	{
 		if (num == nums[i] || num == "all")
 		{
 			digitalWrite(leds[i], HL);
-			break;
 		}
 	}
 }
